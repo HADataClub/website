ee7b669d9a662b8219b3b4c4459ec44b133b588e
W. Edwin Harris <weh9000@gmail.com>
2025-10-20 21:08:46 +0100

    Update script template by renaming sections for clarity and removing exploratory data analysis plots to streamline content. Adjusted table of contents and section numbering for improved organization.

--- files/2025-10-21/script_template.R
+++ files/2025-10-21/script_template.R
@@ -6,11 +6,10 @@
 ## CONTENTS ####
 # 01 DATA PREP AND SETUP
 # 02 EDA
-# 02 ANALYSIS
+# 03 ANALYSIS
 
 
 # 01 DATA PREP AND SETUP ####
-
 # This here is me loading the necessary packages and libraries
 library(package_name)
 
@@ -18,7 +17,7 @@ library(package_name)
 
 mydata <- read.csv("path/to/mydata.csv")
 names(mydata)
-head(mydata)
+str(mydata)
 
 # This here is me converting variable classes if necessary
 mydata$my_factor_variable <- factor(mydata$my_factor_variable)
@@ -26,43 +25,11 @@ mydata$my_numeric_variable <- as.numeric(mydata$my_numeric_variable)
 
 
 # 02 EDA ####
-
 # This here is my EDA
-mosaicplot(table(wilt$crop, wilt$wilt_bin), 
-           color = c("darkgreen", "goldenrod"),
-           ylab = "Wilting present",
-           main = "Rotation crop")
-mosaicplot(table(wilt$amendment, wilt$wilt_bin), 
-           color = c("darkgreen", "goldenrod"),
-           ylab = "Wilting present",
-           main = "Amendment")
-mosaicplot(table(wilt$block, wilt$wilt_bin), 
-           color = c("darkgreen", "goldenrod"),
-           ylab = "Wilting present",
-           main = "Block")
+# This will typically include bivariate plots of the response variable vs predictor variables, 
+# and maybe some multivariate plots of the data
 
-# 02 ANALYSIS ####
 
+# 03 ANALYSIS ####
 # This here is my analysis
-# install.packages("lme4")
-library(lme4) # Statistician's preference
-# install.packages("lmerTest")
-library(lmerTest) # Calculates your p-vals in case CI is not good enough for you
 
-lme1 <- glmer(wilt_bin ~ crop + amendment + (1|block), 
-            data = wilt,
-            family = binomial(link = "logit") )
-
-print(lme1, corr=F)
-summary(lme1)
-
-se <- sqrt(diag(vcov(lme1)))
-# table of estimates with 95% CI
-(tab <- cbind(Est = fixef(lme1), LL = fixef(lme1) - 1.96 * se, UL = fixef(lme1) + 1.96 *
-                se))
-
-# install.packages("sjPlot")
-library(sjPlot)
-
-plot_model(lme1, type = "pred", terms = c("crop"))
-plot_model(lme1, type = "pred", terms = c("amendment"))
