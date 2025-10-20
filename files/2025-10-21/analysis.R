99276b7ca7959b60e3203279802a3906e7a4850b
W. Edwin Harris <weh9000@gmail.com>
2025-10-20 07:48:03 +0100

    Update table of contents location in report_template.qmd from left to right for improved layout.

--- files/2025-10-21/analysis.R
+++ files/2025-10-21/analysis.R
@@ -1,3 +1,15 @@
+## HEADER ####
+# who: Ed H
+# when: 2025-10-21
+# what: Ed vs AI analysis
+
+## CONTENTS ####
+# 01 DATA PREP
+# 02 EDA
+# 02 ANALYSIS
+
+
+# 01 DATA PREP ####
 
 # This here is me getting and prepping the data for analysis
 # install.packages("agricolae")
@@ -14,10 +26,12 @@ names(wilt)
 table(wilt$wilt_percent)
 wilt$block <- factor(wilt$block)
 
+
 # This here creates a binary version of the wilt var
 wilt$wilt_bin <- wilt$wilt_percent
 wilt$wilt_bin[wilt$wilt_bin == 50] <- 1
 
+# 02 EDA ####
 
 # This here is my EDA
 mosaicplot(table(wilt$crop, wilt$wilt_bin), 
@@ -33,26 +47,28 @@ mosaicplot(table(wilt$block, wilt$wilt_bin),
            ylab = "Wilting present",
            main = "Block")
 
+# 02 ANALYSIS ####
+
 # This here is my analysis
 # install.packages("lme4")
 library(lme4) # Statistician's preference
 # install.packages("lmerTest")
 library(lmerTest) # Calculates your p-vals in case CI is not good enough for you
 
-mod <- glmer(wilt_bin ~ crop + amendment + (1|block), 
+lme1 <- glmer(wilt_bin ~ crop + amendment + (1|block), 
             data = wilt,
             family = binomial(link = "logit") )
 
-print(mod, corr=F)
-summary(mod)
+print(lme1, corr=F)
+summary(lme1)
 
-se <- sqrt(diag(vcov(mod)))
+se <- sqrt(diag(vcov(lme1)))
 # table of estimates with 95% CI
-(tab <- cbind(Est = fixef(mod), LL = fixef(mod) - 1.96 * se, UL = fixef(mod) + 1.96 *
+(tab <- cbind(Est = fixef(lme1), LL = fixef(lme1) - 1.96 * se, UL = fixef(lme1) + 1.96 *
                 se))
 
-install.packages("sjPlot")
+# install.packages("sjPlot")
 library(sjPlot)
 
-plot_model(mod, type = "pred", terms = c("crop"))
-plot_model(mod, type = "pred", terms = c("amendment"))
+plot_model(lme1, type = "pred", terms = c("crop"))
+plot_model(lme1, type = "pred", terms = c("amendment"))
