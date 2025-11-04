c4e57176d481bdb33c25e94456651ad0f0e1e803
W. Edwin Harris <weh9000@gmail.com>
2025-11-04 13:14:32 +0000

    Refactor biodiversity_shiny_app.R to replace bounding box inputs with a county text input for improved user interaction, updating the search_occurrences function accordingly to enhance geographical search capabilities.

--- files/2025-11-04/biodiversity_shiny_app.R
+++ files/2025-11-04/biodiversity_shiny_app.R
@@ -20,14 +20,11 @@ require(leaflet)
 
 # Define function to search for occurrences of 
 # specified clades within a polygon (i.e, bounding box=bbox)
-search_occurrences <- function(bbox, clade) {
+search_occurrences <- function(county, clade) {
   occ_search_result <- occ_search(
-    geometry = paste("POLYGON((", bbox["min_longitude"], " ", bbox["min_latitude"], ",", 
-                     bbox["min_longitude"], " ", bbox["max_latitude"], ",", 
-                     bbox["max_longitude"], " ", bbox["max_latitude"], ",", 
-                     bbox["max_longitude"], " ", bbox["min_latitude"], ",", 
-                     bbox["min_longitude"], " ", bbox["min_latitude"], "))"),
-    month = c(1:12), # define months of the year
+    stateProvince = county,
+    country = "GB",
+    month = c(1:12),
     scientificName = clade,
     hasCoordinate = TRUE
   )
@@ -47,10 +44,7 @@ ui <- fluidPage(
                   # First clade to be shown in the drop down box
                   selected = "Aves"), 
       # By default you will have the approximate borders of Shropshire
-      numericInput("min_longitude", "Minimum Longitude:", value = -3.1),
-      numericInput("max_longitude", "Maximum Longitude:", value = -1.9),
-      numericInput("min_latitude", "Minimum Latitude:", value = 52.3),
-      numericInput("max_latitude", "Maximum Latitude:", value = 53.1)
+      textInput("county", "Enter UK County:", value = "Shropshire")
     ),
     mainPanel(
       leafletOutput("map")
@@ -65,14 +59,11 @@ server <- function(input, output) {
   # Render the leaflet map based on user's clade selection and polygon coordinates
   output$map <- renderLeaflet({
     clade <- input$clade
-    bbox <- c(
-      min_longitude = input$min_longitude,
-      min_latitude = input$min_latitude,
-      max_longitude = input$max_longitude,
-      max_latitude = input$max_latitude
-    )
-    
-    occ_search_result <- search_occurrences(bbox, clade)
+    county <- input$county
+    occ_search_result <- search_occurrences(county, clade)
+    if (is.null(occ_search_result$data)) {
+      return(leaflet() %>% addTiles() %>% setView(lng = 0, lat = 51.5, zoom = 10))
+    }
     
     leaflet() %>%
       addTiles() %>%
