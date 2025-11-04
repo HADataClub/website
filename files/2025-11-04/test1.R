222719aaf9c1aeab5fd8a7b81a4a3b6cf626d053
W. Edwin Harris <weh9000@gmail.com>
2025-11-04 13:39:41 +0000

    Refactor occ_search call in test1.R to utilize GADM code for Shropshire, enhancing geographical accuracy and limiting results to 500 occurrences for improved data handling.

--- files/2025-11-04/test1.R
+++ files/2025-11-04/test1.R
@@ -1,24 +1,16 @@
 
 library(rgbif)
 
-bbox <- c(min_longitude = -3.1,
-          max_longitude = -1.9, 
-          min_latitude = 52.3,
-          max_latitude = 53.1)
-
 
 clade <- "Aves"
+gadm_code <- "GBR.1.84.1_1" # Shropshire GADM code
 
-x<- occ_search(
-  geometry = paste("POLYGON((", bbox["min_longitude"], " ", bbox["min_latitude"], ",", 
-                   bbox["min_longitude"], " ", bbox["max_latitude"], ",", 
-                   bbox["max_longitude"], " ", bbox["max_latitude"], ",", 
-                   bbox["max_longitude"], " ", bbox["min_latitude"], ",", 
-                   bbox["min_longitude"], " ", bbox["min_latitude"], "))"),
-  month = c(1:12), # define months of the year
-  year = 2025,
+occ_search_result <- occ_search(
+  gadmGid = gadm_code,
   scientificName = clade,
-  hasCoordinate = TRUE
+  hasCoordinate = TRUE,
+  limit = 500
 )
+return(occ_search_result)
 
-x$meta$count  # See how many records matched
+search_occurrences$meta$count  # See how many records matched
