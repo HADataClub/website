6767070e6f4b7fbbdc0de41d56c9caf0aeb75726
W. Edwin Harris <weh9000@gmail.com>
2025-11-04 12:46:45 +0000

    Enhance user experience in biodiversity_shiny_app.R by updating the GBIF search function to include error handling and improving the layout of the Shiny UI for better accessibility.

---
+++ files/2025-11-04/test1.R
@@ -0,0 +1,21 @@
+
+library(rgbif)
+
+bbox <- c(min_longitude = -3.1,
+          max_longitude = -1.9, 
+          min_latitude = 52.3,
+          max_latitude = 53.1)
+
+
+clade <- "Aves"
+
+x<- occ_search(
+  geometry = paste("POLYGON((", bbox["min_longitude"], " ", bbox["min_latitude"], ",", 
+                   bbox["min_longitude"], " ", bbox["max_latitude"], ",", 
+                   bbox["max_longitude"], " ", bbox["max_latitude"], ",", 
+                   bbox["max_longitude"], " ", bbox["min_latitude"], ",", 
+                   bbox["min_longitude"], " ", bbox["min_latitude"], "))"),
+  month = c(1:12), # define months of the year
+  scientificName = clade,
+  hasCoordinate = TRUE
+)
