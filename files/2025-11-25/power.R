d84834c1d727e9825faa98d042022126c357e95a
W. Edwin Harris <weh9000@gmail.com>
2025-11-25 13:47:05 +0000

    update script

--- files/2025-11-25/power.R
+++ files/2025-11-25/power.R
@@ -1,11 +1,13 @@
 ## Info ####
-## what: Sophie power white stork
-## when: 2025=11-21
+## what: HADC Sophie power white stork
+## when: 2025-11-25
 
 ## Contents ####
 ## 1 setup
 ## 2 Estimates
 ## 3 Power
+## 4 LME models and effect size
+
 
 ## 1 setup ####
 install.packages("pwr")
@@ -43,6 +45,27 @@ nM <- 10
 pwr.t.test(n = 19/2, d = cohen_d, sig.level = 0.05, power = NULL)
 
 pwr.t.test(n = NULL, d = cohen_d, sig.level = 0.05, power = 0.8)
+
+
+## 4 LME models and effect size ####
+
+# library(lme4)
+# fit <- lmer(RT ~ prime + (prime|item) + (prime|participant), data = adelman)
+# summary(fit)
+
+# difference between means
+mydiff <- 16.029
+
+var_int_part <- 10032.3
+var_int_item <- 1900.12
+var_slope_part <- 27.89
+var_slope_item <- 19.88
+
+var_resid <- 22128.15
+
+# d <- difference / sqrt((all random variance components added up + residual variance))
+(d <- mydiff / sqrt((var_int_part + var_int_item + var_slope_part + var_slope_item + var_resid)))
+
 
 
 
