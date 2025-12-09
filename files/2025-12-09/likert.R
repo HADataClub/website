b01781ada75eaba3b93063e95842bc7c36a7822f
W. Edwin Harris <weh9000@gmail.com>
2025-12-09 13:47:11 +0000

    Update hadc_schedule.html and search.json to include updated links for Meetings #198 and #199 slides, improving accessibility to presentation materials and ensuring accurate information for upcoming events.

---
+++ files/2025-12-09/likert.R
@@ -0,0 +1,21 @@
+## Info ####
+## who: Ed H
+## what: HADC! Likert viz
+## when: 2025-12-09
+
+## Contents ####
+## 01 Setup
+## 02 Data exploration
+## 03 Graphing options
+## 04 References
+
+## 01 Setup ####
+
+
+## 02 Data exploration ####
+
+
+## 03 Graphing options ####
+
+
+## 04 References ####
